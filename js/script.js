de814772dc3371f1d0f0dcb0c09227569e9065ac
Lucca Bevilacqua <luccagomes2@gmail.com>
2023-08-08 20:42:00 +0200

    Initial commit. Early version of page

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+if (document.getElementById('experience-section-link')) {
+  document.getElementById('experience-section-link').addEventListener('click', () => {
+    document.getElementById('experience-section').scrollIntoView({behavior: "smooth"})
+  })
+}
