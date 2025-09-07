652b71ca996cb5d341fdd14c12cb8ea3fc8ad23e
bglucca <57628729+bglucca@users.noreply.github.com>
2025-09-07 18:33:22 +0000

    Deploying to gh-pages from @ bglucca/bglucca.github.io@746f7e2bb39bc25e5dd98ea008b9cd382c4b0947 🚀

--- assets/js/fusejs/fusejs-store.js
+++ assets/js/fusejs/fusejs-store.js
@@ -4,6 +4,16 @@ MIT License
 From minimal-mistakes: https://github.com/mmistakes/minimal-mistakes/
 */
 var store = [{
+        "title": "About Me",
+        "subtitle": "",
+        "excerpt": "",
+        "content": "Hey there!👋 I’m Lucca. I'm a brazilian engineer turned Data Scientist, after noticing that all my favorite subjects revolved around Data and Statistics. I've worked for many years in Brazil...",
+        "categories": null,
+        "date": null,
+        "tags": null,
+        "url": "https://bglucca.github.io/aboutme",
+        "img": null
+      },{
         "title": "Archive",
         "subtitle": "",
         "excerpt": "",
@@ -11,137 +21,47 @@ var store = [{
         "categories": null,
         "date": null,
         "tags": null,
-        "url": "https://dieghernan.github.io/archive",
+        "url": "https://bglucca.github.io/archive",
         "img": null
       },{
-        "title": "First post!",
+        "title": "Browsing AirBnb using Data Science",
         "subtitle": "",
-        "excerpt": "<p>This is my first post, how exciting!</p>",
-        "content": "This is my first post, how exciting!",
+        "excerpt": "<p>I consider myself somewhat of a regular traveler. That’s why I often look for places to stay in AirBnb. But, when I fire it up, the options are countless.Don’t get me wrong, the filters on the site are great and I always liked it. But as a Data Scientist, I wonder if even with some simple Data Analysis navigating AirBnB could be made easier.</p>",
+        "content": "I consider myself somewhat of a regular traveler. That’s why I often look for places to stay in AirBnb. But, when I fire it up, the options are countless. Don’t...",
         "categories": [],
-        "date": "2015-01-04",
-        "tags": ["random","exciting-stuff"],
-        "url": "https://dieghernan.github.io/20150104_first-post/",
-        "img": null
+        "date": "2022-11-12",
+        "tags": ["Unsupervised Learning","EDA","Data Analysis","Data Science","Case Study"],
+        "url": "https://bglucca.github.io/20221112_rio-airbnb/",
+        "img": "https://bglucca.github.io/assets/img/rio_airbnb/cover-image.jpg"
       },{
-        "title": "Pirates arrrr",
-        "subtitle": "",
-        "excerpt": "<p>Piracy is typically an act of robbery or criminal violence at sea. The term can include acts committed on land, in the air, or in other major bodies of water or on a shore. It does not normally include crimes committed against persons traveling on the same vessel as the perpetrator (e.g. one passenger stealing from others on the same vessel). The term has been used throughout history to refer to raids across land borders by non-state agents.</p>",
-        "content": "Piracy is typically an act of robbery or criminal violence at sea. The term can include acts committed on land, in the air, or in other major bodies of water...",
+        "title": "You’ve got mail! Machine Learning for Customer Segmentation.",
+        "subtitle": "Using Data Science to better understand the customers for a mail-order company and predicting their marketing actions’ response.",
+        "excerpt": "<p>Table of Contents</p>",
+        "content": "Table of Contents Who should I reach out to? And where are they? Initial EDA Building Customer Segmentation Predicting Customer Response Conclusions Disclaimer: This article and data for the development of the...",
         "categories": [],
-        "date": "2015-01-15",
-        "tags": ["pirates","random","wikipedia"],
-        "url": "https://dieghernan.github.io/20150115_pirates/",
-        "img": null
+        "date": "2023-08-07",
+        "tags": ["Machine Learning","Customer Segmentation","Supervised Learning","Unsupervised Learning","Case Study"],
+        "url": "https://bglucca.github.io/20230807_customer-segmentation/",
+        "img": "https://bglucca.github.io/assets/img/customer-segmentation/cover-image.webp"
       },{
-        "title": "Soccer",
-        "subtitle": "Best sport ever!",
-        "excerpt": "<p>From Wikipedia:</p>",
-        "content": "From Wikipedia: Association football, more commonly known as football or soccer,[2] is a sport played between two teams of eleven players with a spherical ball. It is played by 250...",
+        "title": "MLflow 101 - My Quickstart",
+        "subtitle": "A gentle introduction to MLFlow and its applications with Hyperparameter tuning",
+        "excerpt": "<p>If you’ve been working with Data Science I’m sure that at some point you needed to try a lot of different things to solve a problem. At the same time, you also needed to keep track of the performance of what you tried to do.</p>",
+        "content": "If you’ve been working with Data Science I’m sure that at some point you needed to try a lot of different things to solve a problem. At the same time,...",
         "categories": [],
-        "date": "2015-01-19",
-        "tags": ["random","exciting-stuff","wikipedia"],
-        "url": "https://dieghernan.github.io/20150119_soccer/",
-        "img": null
-      },{
-        "title": "Dear diary",
-        "subtitle": "",
-        "excerpt": "<p>What is it with that Mary girl?  Dragging me to school every day. As if I had a choice.  What you don’t hear in those nursery rhymes is that she starves me if I don’t go to school with her; it’s the only way I can stay alive!  I’m thinking about being adopted by Little Bo Peep, sure I may get lost, but anything is better than being with Mary and those little brats at school (shudder, shudder).</p>",
-        "content": "What is it with that Mary girl? Dragging me to school every day. As if I had a choice. What you don’t hear in those nursery rhymes is that she...",
-        "categories": [],
-        "date": "2015-01-27",
-        "tags": ["random","diary","school","manuscript"],
-        "url": "https://dieghernan.github.io/20150127_dear-diary/",
-        "img": null
-      },{
-        "title": "To be",
-        "subtitle": "… or not to be?",
-        "excerpt": "<p>To be, or not to be–that is the question:Whether ‘tis nobler in the mind to sufferThe slings and arrows of outrageous fortuneOr to take arms against a sea of troublesAnd by opposing end them. To die, to sleep–No more–and by a sleep to say we endThe heartache, and the thousand natural shocksThat flesh is heir to. ‘Tis a consummationDevoutly to be wished. To die, to sleep–To sleep–perchance to dream: ay, there’s the rub,For in that sleep of death what dreams may comeWhen we have shuffled off this mortal coil,Must give us pause. There’s the respectThat makes calamity of so long life.For who would bear the whips and scorns of time,Th’ oppressor’s wrong, the proud man’s contumelyThe pangs of despised love, the law’s delay,The insolence of office, and the spurnsThat patient merit of th’ unworthy takes,When he himself might his quietus makeWith a bare bodkin? Who would fardels bear,To grunt and sweat under a weary life,But that the dread of something after death,The undiscovered country, from whose bournNo traveller returns, puzzles the will,And makes us rather bear those ills we haveThan fly to others that we know not of?Thus conscience does make cowards of us all,And thus the native hue of resolutionIs sicklied o’er with the pale cast of thought,And enterprise of great pitch and momentWith this regard their currents turn awryAnd lose the name of action. – Soft you now,The fair Ophelia! – Nymph, in thy orisonsBe all my sins remembered.</p>",
-        "content": "To be, or not to be–that is the question: Whether ‘tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a...",
-        "categories": [],
-        "date": "2015-02-13",
-        "tags": ["wikipedia","manuscript"],
-        "url": "https://dieghernan.github.io/20150213_hamlet-monologue/",
-        "img": null
-      },{
-        "title": "Test markdown",
-        "subtitle": "Each post also has a subtitle",
-        "excerpt": "<p>You can write regular markdown here and Jekyll will automatically convert it to a nice webpage.  I strongly encourage you to take 5 minutes to learn how to write in markdown - it’ll teach you how to transform regular text into bold/italics/headings/tables/etc.</p>",
-        "content": "You can write regular markdown here and Jekyll will automatically convert it to a nice webpage. I strongly encourage you to take 5 minutes to learn how to write in...",
-        "categories": [],
-        "date": "2015-02-20",
-        "tags": ["markdown","exciting-stuff"],
-        "url": "https://dieghernan.github.io/20150220_test-markdown/",
-        "img": null
-      },{
-        "title": "Flake it till you make it",
-        "subtitle": "Excerpt from Soulshaping by Jeff Brown",
-        "excerpt": "<p>Under what circumstances should we step off a path? When is it essential that we finish what we start? If I bought a bag of peanuts and had an allergic reaction, no one would fault me if I threw it out. If I ended a relationship with a woman who hit me, no one would say that I had a commitment problem. But if I walk away from a seemingly secure route because my soul has other ideas, I am a flake?</p>",
-        "content": "Under what circumstances should we step off a path? When is it essential that we finish what we start? If I bought a bag of peanuts and had an allergic...",
-        "categories": [],
-        "date": "2015-02-26",
-        "tags": ["random","manuscript"],
-        "url": "https://dieghernan.github.io/20150226_flake-it-till-you-make-it/",
-        "img": null
-      },{
-        "title": "Urban art",
-        "subtitle": "Creativity is everywhere",
-        "excerpt": "<p>Urban artists can use anything as a canvas to express their art</p>",
-        "content": "Urban artists can use anything as a canvas to express their art      Picture by Zarateman",
-        "categories": ["picture"],
-        "date": "2018-11-09",
-        "tags": ["urban art","street","downtown","image"],
-        "url": "https://dieghernan.github.io/20181109_urban_art/",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Madrid_-_Graffiti_11.jpg/407px-Madrid_-_Graffiti_11.jpg"
-      },{
-        "title": "Dean Village",
-        "subtitle": "",
-        "excerpt": "<p>Dean Village (from dene, meaning ‘deep valley’) is a former village immediately northwest of the city centre of Edinburgh, Scotland. It was known as the “Water of Leith Village” and was the centre of a successful grain milling area for more than 800 years. At one time there were no fewer than eleven working mills there, driven by the strong currents of the Water of Leith.  From Wikipedia</p>",
-        "content": "Dean Village (from dene, meaning ‘deep valley’) is a former village immediately northwest of the city centre of Edinburgh, Scotland. It was known as the “Water of Leith Village” and...",
-        "categories": ["picture"],
-        "date": "2019-05-02",
-        "tags": ["landscape","trips","downtown","edinburgh","kramdown","bootstrap","wikipedia","demo","image"],
-        "url": "https://dieghernan.github.io/20190502_dean-village/",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Well_Court%2C_Dean_Village%2C_Edinburgh_%2844485996381%29.jpg/800px-Well_Court%2C_Dean_Village%2C_Edinburgh_%2844485996381%29.jpg"
-      },{
-        "title": "A landing page",
-        "subtitle": "With image header",
-        "excerpt": "<p>A simple landing page with an image on top. Transparencies (png) works better on this combination.</p>",
-        "content": "A simple landing page with an image on top. Transparencies (png) works better on this combination.",
-        "categories": ["landing-page"],
-        "date": "2019-08-03",
-        "tags": ["example","demo","layout","guest-author","image"],
-        "url": "https://dieghernan.github.io/20190803_landing-page/",
-        "img": "https://dieghernan.github.io/chulapa/assets/img/site/transparent.png"
-      },{
-        "title": "The Acueduct of Segovia",
-        "subtitle": "20 centuries ago",
-        "excerpt": "<p>The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and the foremost symbol of Segovia, as evidenced by its presence on the city’s coat of arms.</p>",
-        "content": "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and...",
-        "categories": ["picture"],
-        "date": "2019-11-27",
-        "tags": ["landscape","trips","downtown","project-links","wikipedia","image"],
-        "url": "https://dieghernan.github.io/20191127_acueduct-of-segovia/",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Acueducto_Segovia_noche.JPG/1024px-Acueducto_Segovia_noche.JPG"
-      },{
-        "title": "Current skin",
-        "subtitle": "Showing al the different Bootstrap components available",
-        "excerpt": "<p>This is a demo page showing the different components of Bootstrap and how they look on this site under the current configuration.</p>",
-        "content": "This is a demo page showing the different components of Bootstrap and how they look on this site under the current configuration. Previous The Acueduct of Segovia The Acueduct o......",
-        "categories": ["skins"],
-        "date": "2020-12-31",
-        "tags": ["skin","bootstrap","current-theme","header-hero","image","demo"],
-        "url": "https://dieghernan.github.io/20201231_current-skin/",
-        "img": "https://picsum.photos/id/1018/2000/2000"
+        "date": "2023-10-12",
+        "tags": ["MLOps","MLFlow","Monitoring"],
+        "url": "https://bglucca.github.io/20231012_mlflow-quickstart/",
+        "img": "https://bglucca.github.io/assets/img/mlflow-quickstart/cover-image.jpg"
       },{
         "title": "Markdown cheatsheet",
         "subtitle": "The basic syntax of your page",
         "excerpt": "<p>A demo page checking Markdown</p>",
         "content": "What’s Markdown? Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents. Created by John Gruber in 2004, Markdown is now one...",
         "categories": ["tutorial"],
-        "date": "2025-08-14",
+        "date": "2025-09-07",
         "tags": ["starter","syntax","markdown"],
-        "url": "https://dieghernan.github.io/cheatsheets/01-markdown-cheat-sheet",
+        "url": "https://bglucca.github.io/cheatsheets/01-markdown-cheat-sheet",
         "img": null
       },{
         "title": "kramdown cheatsheet",
@@ -149,8 +69,8 @@ var store = [{
         "excerpt": "<p>kramdown supercharges Markdown with some interesting features.</p>",
         "content": "What’s kramdown? kramdown supercharges Markdown with some interesting features. kramdown is the default Jekyll Markdown processor. When creating your site with Jekyll, you can use the standard Markdown syntax plus...",
         "categories": ["tutorial"],
-        "date": "2025-08-14",
+        "date": "2025-09-07",
         "tags": ["starter","syntax","kramdown"],
-        "url": "https://dieghernan.github.io/cheatsheets/02-kramdown-cheat-sheet",
+        "url": "https://bglucca.github.io/cheatsheets/02-kramdown-cheat-sheet",
         "img": null
       }]
